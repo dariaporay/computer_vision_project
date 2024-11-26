2e60406e8f2173f1c5f6e0a6a9039c0c3a41c788
dariaporay <daryaporay@gmail.com>
2024-11-26 09:28:11 +0300

    добавлены комментарии, подправлены ограничения на рисование

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ handsDetector = mp.solutions.hands.Hands(static_image_mode=False,
                    max_num_hands=1,
                    min_detection_confidence=0.7,
                    min_tracking_confidence=0.7)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 ret, frame = cap.read()
 h, w, _ = frame.shape
 print(h, w)
@@ -19,10 +19,10 @@ fl_figure = 0
 fl = False
 x_const = 0
 y_const = 0
-color = (0, 255, 0)
 eps = 20
 size = 3
 k = 1
+# распознование указательного пальца взято из канваса
 while cap.isOpened():
     ret, frame = cap.read()
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
@@ -32,9 +32,9 @@ while cap.isOpened():
     flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
     # Распознаем
     results = handsDetector.process(flippedRGB)
-    # Рисуем распознанное, если распозналось
+    # анализируем изображение, если распозналось
     if results.multi_hand_landmarks is not None:
-
+        # понимаем какая рука: левая или правая
         hand = results.multi_handedness[0].classification[0].label
         if hand == "Left":
             k = -1
@@ -43,7 +43,7 @@ while cap.isOpened():
 
         mp.solutions.drawing_utils.draw_landmarks(flippedRGB, results.multi_hand_landmarks[0])
 
-        # нас интересует только подушечка указательного пальца (индекс 8)
+        # находим координате тех, точек которые нам нужны
         # нужно умножить координаты а размеры картинки
         x_index = int(results.multi_hand_landmarks[0].landmark[8].x *
                 flippedRGB.shape[1])
@@ -83,9 +83,12 @@ while cap.isOpened():
                        flippedRGB.shape[0])
         y_1 = int(results.multi_hand_landmarks[0].landmark[1].y *
                        flippedRGB.shape[0])
-        if y_17 - y_0 > 0 or k * (x_13 - x_17) > 5 or (y_1 - y_0) > 8:
+        # ставим ограничения на положение руки во время рисования
+        if y_17 - y_0 > 10 or k * (x_13 - x_17) > 10 or (y_1 - y_0) > 10:
             fl = False
+        # ограничения пройдены, можно рисовать
         else:
+            # проверка близости указательного и большого пальца для рисования окружности на данном кадре
             if abs(x_big - x_index) < eps and abs(y_big - y_index) < eps:
                 if fl:
                     cv2.circle(flippedRGB, (x_const, y_const), int(math.hypot(x_const - x_index, y_const - y_index)),
@@ -96,6 +99,7 @@ while cap.isOpened():
                     x_const, y_const = x_index, y_index
                     cv2.circle(flippedRGB, (x_index, y_index), size, (0, 0, 255), -1)
 
+            # проверка близости безымянного и указательного пальца для рисования отрезка на данном кадре
             elif abs(x_noname - x_index) < eps and abs(y_noname - y_index) < eps:
                 if fl:
                     cv2.line(flippedRGB, (x_const, y_const), (x_index, y_index), (255, 0, 0), size)
@@ -105,12 +109,15 @@ while cap.isOpened():
                     x_const, y_const = x_index, y_index
                     cv2.circle(flippedRGB, (x_index, y_index), size, (255, 0, 0), -1)
 
+            # проверка близости среднего и большого пальца для стирания уже нарисованного на вспомогательном холсте
             elif abs(x_big - x_middle) < eps and abs(y_big - y_middle) < eps:
                 cv2.circle(helped_picture, (x_index, y_index), 20, (0, 0, 0), -1)
 
+            # проверка близости мизинца и большого пальца для обычного рисования на вспомогателном холсте
             elif abs(x_big - x_little) < eps and abs(y_big - y_little) < eps:
                 cv2.circle(helped_picture, (x_index, y_index), size, (0, 255, 0), -1)
 
+            # фиксирование конечной версии отрезка или окружности на вспомогательный холст
             else:
                 if fl:
                     if fl_figure == 1:
@@ -121,13 +128,12 @@ while cap.isOpened():
                 fl = False
                 fl_figure = 0
 
-    # переводим в BGR и показываем результат
+    # переводим в BGR, накладываем нарисованный материал и показываем результат
     helped_picture[h - 60:, :] = picture_no_draw_zone
     helped_mask = cv2.cvtColor(helped_picture, cv2.COLOR_BGR2GRAY)
     ret_inv, tr_inv = cv2.threshold(helped_mask, 10, 255, cv2.THRESH_BINARY_INV)
     flipped_BGR = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
     res_image = cv2.bitwise_and(flipped_BGR, flipped_BGR, mask=tr_inv)
-    cv2.imshow("picture_no_draw_zone", picture_no_draw_zone)
     cv2.imshow("Hands", res_image + helped_picture)
 
 # освобождаем ресурсы
